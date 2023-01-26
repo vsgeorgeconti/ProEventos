4050123396a6a2a1bfe6d5a0a340a3131baceef7
George <george.conti@datapar.com.br>
2023-01-26 14:39:59 -0300

    Ajuste no front

--- FRONT/ProEventos-App/src/app/eventos/eventos.component.ts
+++ FRONT/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImg = 150;
+  margemImg = 2;
+  exibirImg = true;
+  private _filtroLista: string = ''
 
+  public get filtroLista(){
+    return this._filtroLista
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.exibirImg = !this.exibirImg;
+  }
+
   public getEventos() : void {
       this.http.get('https://localhost:7152/api/eventos').subscribe(
-        response => this.eventos = response,
+        response => {
+          this.eventos = response
+          , this.eventosFiltrados = response
+        },
         error => console.log(error)
       );
   }
